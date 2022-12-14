ff70fe641fce6dbb6c2de05416306bda97613fbd
Albert Tang <tangalbert919@yahoo.com>
2022-12-14 16:18:32 -0600

    negroni: overlay: Decommonize CarrierConfigRes
    Change-Id: Id681f3525c1e69871b58c71e2ef3f4b1d0dbedf9

--- device.mk
+++ device.mk
@@ -33,6 +33,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    CarrierConfigResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSystemUIResTarget
